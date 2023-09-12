7ed5ea60860d998fb039da5b293fc33418cced7b
balae <balaev.job@gmail.com>
2023-09-12 17:34:44 +0300

    test

--- src/main/java/com/mystite/blog/repositories/UserRepository.java
+++ src/main/java/com/mystite/blog/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.mystite.blog.repositories;
 import com.mystite.blog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
